009ebb3bdd47812c0fa7dbe0d8d136b7025785d6
Luis Octavio <octaviodevtech@gmail.com>
2024-05-19 21:57:10 -0400

    Add: Controladores

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,4 +1,54 @@
-import { Controller } from "@nestjs/common";
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Body,
+  Param,
+  NotFoundException,
+} from '@nestjs/common';
+import { TaskService } from './task.service';
+import { Task } from '@prisma/client';
 
-@Controller()
-export class TaskController{}
+//CONTROLADORES EN NEST
+@Controller('tasks')
+export class TaskController {
+  constructor(private readonly taskService: TaskService) {}
+
+  @Get()
+  async getAllTasks() {
+    return this.taskService.getAllTasks();
+  }
+
+  @Post()
+  async createTask(@Body() data: Task) {
+    return this.taskService.createTask(data);
+  }
+
+  @Get(':id')
+  async getTaskById(@Param('id') id: string) {
+    const taskFound = await this.taskService.getTaskById(Number(id));
+    if (!taskFound) throw new NotFoundException('Task does not exists');
+    return taskFound;
+  }
+
+  @Delete(':id')
+  async deleteTaskById(@Param('id') id: string) {
+    try {
+      return await this.taskService.deleteTask(Number(id));
+    } catch (error) {
+      throw new NotFoundException('Task does not exists');
+    }
+  }
+
+  @Put(':id')
+  async updateTaskById(@Param('id') id: string, @Body() data: Task) {
+      try {
+          
+          return await this.taskService.updateTask(Number(id), data);
+    } catch (error) {
+        throw new NotFoundException("Task does not exists")
+    }
+  }
+}
--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,12 +1,48 @@
-import { Injectable } from "@nestjs/common";
-import { PrismaServices } from "src/prisma/prisma.services";
-import { Task } from "@prisma/client";
+import { Injectable } from '@nestjs/common';
+import { PrismaServices } from 'src/prisma/prisma.services';
+import { Task } from '@prisma/client';
 
 @Injectable()
-export class TaskService{
-    constructor(private primsa: PrismaServices){}
+export class TaskService {
+  constructor(private primsa: PrismaServices) {}
 
-    async getAllTasks(): Promise<Task[]>{
-        return this.primsa.task.findMany();
-    }
+  //CREACION DE METODO PARA TRAER UNA LISTA DE TAREAS
+  async getAllTasks(): Promise<Task[]> {
+    return this.primsa.task.findMany();
+  }
+
+  //METODO PARA TRAER UNA TAREA EN BASE A UN ID
+  async getTaskById(id: number): Promise<Task> {
+    return this.primsa.task.findUnique({
+      where: {
+        id,
+      },
+    });
+  }
+
+  //CREACION DE TAREAS
+  async createTask(data: Task): Promise<Task> {
+    return this.primsa.task.create({
+      data: data,
+    });
+  }
+
+  //ACTUALIZACION DE TAREAS
+  async updateTask(id: number, data: Task): Promise<Task> {
+    return this.primsa.task.update({
+      where: {
+        id,
+      },
+      data,
+    });
+  }
+
+  //ELIMINACION DE TAREAS
+  async deleteTask(id: number): Promise<Task> {
+    return this.primsa.task.delete({
+      where: {
+        id,
+      },
+    });
+  }
 }
